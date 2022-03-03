e88424e8e705a33f7293e542b7b1238cbf444a00
closebox73 <khikyardana@gmail.com>
2022-03-03 20:52:09 +0700

    Add and Update all config license

--- Jabbah/scripts/weather-icon.sh
+++ Jabbah/scripts/weather-icon.sh
@@ -2,4 +2,4 @@
 
 theme=$1
 weather_code=$2
-cp -r ~/.conky/Nunki/icons/${theme}/${weather_code}.png ~/.cache/weather-icon.png
+cp -r ~/.config/conky/Jabbah/icons/${theme}/${weather_code}.png ~/.cache/weather-icon.png
